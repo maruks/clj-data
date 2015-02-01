404b1debc7278917fc4e6a388451cd0f7cc5a3f6
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2015-02-01 20:53:52 +0000

    added some tests

--- src/maruks/data/leftist_heap.clj
+++ src/maruks/data/leftist_heap.clj
@@ -1,59 +1,76 @@
 (ns maruks.data.leftist-heap)
 
+
+(set! *warn-on-reflection* true)
+
 (declare insert)
 (declare find-min)
 (declare delete-min)
-(declare empty-heap)
+(declare empty-node)
 (declare is-empty?)
 (declare heap-seq)
+(declare count-nodes)
 
-(deftype LeftistHeap [rank elem left right cmpfn]
-  clojure.lang.IPersistentStack
-  (peek [this]
-    (find-min this))
-  (pop [this]
-    (delete-min this))  
-  (cons [this e]
-    (insert this e))
-  (count [this]
-    (if (is-empty? this) 0 (+ 1 (count left) (count right))))
-  (empty [this]
-    (empty-heap cmpfn))
-  (equiv [this o]
-    (= (seq this) (seq o)))
-  clojure.lang.Seqable
-  (seq [this]
-    (when-not (is-empty? this)
-      (heap-seq this)))
+(deftype TreeNode [rank elem left right] 
   Object
-  (toString [this]
-    (clojure.string/join " " (seq this)))
   (hashCode [this]
     (+ (.hashCode rank)
-              (if (nil? elem) 1 (.hashCode elem))              
-              (if (nil? left) 2 (.hashCode left))
-              (if (nil? right) 3 (.hashCode right))))
+       (if (nil? elem) 1 (.hashCode elem))              
+       (if (nil? left) 2 (.hashCode left))
+       (if (nil? right) 3 (.hashCode right))))
   (equals [this o]
     (or
      (identical? this o)
      (and (instance? (class this) o)
           (= (.hashCode this) (.hashCode o))))))
 
-(defn is-empty? [^LeftistHeap h]
+(deftype LeftistHeap [^TreeNode root cmpfn]
+  clojure.lang.IPersistentStack
+  (peek [this]
+    (find-min root))
+  (pop [this]
+    (LeftistHeap. (delete-min root cmpfn) cmpfn))  
+  (cons [this e]
+    (LeftistHeap. (insert root e cmpfn) cmpfn))
+  (count [this]
+    (count-nodes root))
+  (empty [this]
+    (LeftistHeap. (empty-node) cmpfn))
+  (equiv [this o]
+    (= (seq this) (seq o)))
+  clojure.lang.Seqable
+  (seq [this]
+    (when-not (is-empty? root)
+      (heap-seq root cmpfn)))
+  Object
+  (toString [this]
+    (clojure.string/join " " (seq this)))
+  (hashCode [this]
+    (.hashCode root))
+  (equals [this o]
+    (or
+     (identical? this o)
+     (and (instance? (class this) o)
+          (= (.hashCode root) (.hashCode o))))))
+
+(defn count-nodes [^TreeNode n]
+  (if (is-empty? n) 0 (+ 1 (count-nodes (.left n) (.right n)))))
+
+(defn is-empty? [^TreeNode h]
   (nil? (.elem h)))
 
-(defn rank [^LeftistHeap h]
+(defn rank [^TreeNode h]
   (.rank h))
 
-(defn make-node [e ^LeftistHeap h1 ^LeftistHeap h2]
+(defn make-node [e ^TreeNode h1 ^TreeNode h2]
   (if (>= (rank h1) (rank h2))
-    (->LeftistHeap (inc (rank h2)) e h1 h2 (.cmpfn h1))
-    (->LeftistHeap (inc (rank h1)) e h2 h1 (.cmpfn h1))))
+    (->TreeNode (inc (rank h2)) e h1 h2)
+    (->TreeNode (inc (rank h1)) e h2 h1)))
 
-(defn empty-heap [cmpfn]
-  (->LeftistHeap 0 nil nil nil cmpfn))
+(defn empty-node []
+  (->TreeNode 0 nil nil nil))
 
-(defn heap-merge [^LeftistHeap h1 ^LeftistHeap h2]
+(defn heap-merge [^TreeNode h1 ^TreeNode h2 cmpfn]
   (cond (is-empty? h1) h2
         (is-empty? h2) h1
         :else (let [x (.elem h1)
@@ -62,29 +79,28 @@
                     y (.elem h2)                    
                     a2 (.left h2)
                     b2 (.right h2)]
-                (if ((.cmpfn h1) x y) 
-                  (make-node x a1 (heap-merge b1 h2))
-                  (make-node y a2 (heap-merge h1 b2))))))
+                (if (cmpfn  x y) 
+                  (make-node x a1 (heap-merge b1 h2 cmpfn))
+                  (make-node y a2 (heap-merge h1 b2 cmpfn))))))
 
-(defn insert [^LeftistHeap h e]
-  (let [cmpfn (.cmpfn h)]
-    (heap-merge (->LeftistHeap 1 e (empty-heap cmpfn) (empty-heap cmpfn) cmpfn) h)))
+(defn insert [^TreeNode h e cmpfn]
+  (heap-merge (->TreeNode 1 e (empty-node) (empty-node)) h cmpfn))
 
-(defn find-min [^LeftistHeap h]
+(defn find-min [^TreeNode h]
   (when-not (is-empty? h)
     (.elem h)))
 
-(defn delete-min [^LeftistHeap h]
+(defn delete-min [^TreeNode h cmpfn]
   (when-not (is-empty? h)
-    (heap-merge (.left h) (.right h))))
+    (heap-merge (.left h) (.right h) cmpfn)))
 
-(defn heap-seq [^LeftistHeap h]
+(defn heap-seq [^TreeNode h cmpfn]
   (lazy-seq
    (when-not (is-empty? h)
-     (cons (find-min h) (heap-seq (delete-min h))))))
+     (cons (find-min h) (heap-seq (delete-min h cmpfn) cmpfn)))))
 
 (defn heap [cmpfn & xs]
-  (reduce insert (empty-heap cmpfn) xs))
+  (->LeftistHeap (reduce #(insert %1 %2 cmpfn) (empty-node) xs) cmpfn))
 
 (defn min-heap [& xs]
   (apply heap (cons <= xs)))
--- test/maruks/data/leftist_heap_test.clj
+++ test/maruks/data/leftist_heap_test.clj
@@ -2,6 +2,47 @@
   (:require [clojure.test :refer :all]
             [maruks.data.leftist-heap :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest min-heap-test
+
+  (def h (min-heap 7 3 9))
+  (def h2 (min-heap 7 3 9))
+
+  (testing "seqable"
+    (is (= '(3 7 9) (seq h))))
+  
+  (testing "stack functions"
+    (is (= 3 (peek h)))
+    (is (= (list 7 9) (seq (pop h))))
+    (is (= 1 (peek (conj h 1))))
+    (is (= 3 (count h)))
+    (is (not (empty? h)))
+    (is (= h (list 3 7 9)))
+    (is (= h h2)))
+
+  (testing "object functions"
+    (is (not (nil? (.toString h))))
+    (is (= (.equals h h2)))
+    (is (= (.hashCode h) (.hashCode h2)))))
+
+
+(deftest max-heap-test
+
+  (def h (max-heap 7 3 9))
+  (def h2 (max-heap 7 3 9))
+
+  (testing "seqable"
+    (is (= '(9 7 3) (seq h))))
+  
+  (testing "stack functions"
+    (is (= 9 (peek h)))
+    (is (= (list 7 3) (seq (pop h))))
+    (is (= 10 (peek (conj h 10))))
+    (is (= 3 (count h)))
+    (is (not (empty? h)))
+    (is (= h (list 9 7 3)))
+    (is (= h h2)))
+
+  (testing "object functions"
+    (is (not (nil? (.toString h))))
+    (is (= (.equals h h2)))
+    (is (= (.hashCode h) (.hashCode h2)))))
