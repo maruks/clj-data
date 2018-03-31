996283d2e0cb55bf8828e913495db50b648970fb
Maris Orbidans <maris.orbidans@onthemarket.com>
2018-03-31 12:51:54 +0100

    version 0.0.2

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
-(defproject org.clojars.maruks/maruks.data "0.0.2-SNAPSHOT"
+(defproject org.clojars.maruks/maruks.data "0.0.3-SNAPSHOT"
   :description "Persistent data structures"
   :url "https://github.com/maruks/clj-data"
+
   :scm {:name "git"
         :url "https://github.com/maruks/clj-data.git"}
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :repl-options {:port 2323}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [org.clojure/test.check "0.7.0"]]}})
